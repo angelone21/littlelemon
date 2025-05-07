bfa07cf75426d8745203c6c4a7481f3d7e4e522b
Vincenzo Angelone <vincenzo.angelone02@gmail.com>
2025-05-07 09:50:17 +0200

    Added the folder to the repository

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Vincenzo Angelone on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
